b73297e9d5bf7100897de3858f56df0256558ffd
Florisvan der Breggen <florisvanderbreggen@gmail.com>
2018-03-07 13:33:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,27 +3,41 @@ function add(a, b) {
   return a + b;
 }
 
+
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function inc(a) {
+  return a++;
+}
+
+function add(a, b) {
+  return a + b;
+}
+
+function dec(a) {
+  return a--;
+}
+
+function makeInt(n) {
+  return parseInt(n);
+}
+
+function add(a, b) {
+  return a + b;
+}
+
 /*
-it('subtract(a, b) subtracts b from a and returns the result', function() {
-  expect(subtract(a, b)).toEqual(a - b)
-})
-
-it('multiply(a, b) multiplies two numbers and returns the result', function() {
-  expect(multiply(a, b)).toEqual(a * b)
-})
-
-it('divide(a, b) divides a by b and returns the result', function() {
-  expect(divide(a, b)).toEqual(a / b)
-})
-
-it('inc(n) increments n and returns the result', function() {
-  expect(inc(a)).toEqual(a + 1)
-})
-
-it('dec(n) decrements n and returns the result', function() {
-  expect(dec(a)).toEqual(a - 1)
-})
-
 describe('makeInt(n)', function() {
   it('parses n as an integer and returns the parsed integer', function() {
     expect(makeInt(a.toString())).toEqual(a)
